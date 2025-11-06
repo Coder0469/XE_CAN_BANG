a1fb53cd1e095758e3e4013f0927fd1f6263a343
Đình Hiếu <dinhhieu8061@gmail.com>
2025-11-06 09:52:28 +0700

    Implement DCMotor initialization and control functions

---
+++ Library/DCMotor.c
@@ -0,0 +1,36 @@
+
+#include "DCMotor.h"
+
+void DCMotor_Init(DCMotor *motor,int max_speed, TIM_HandleTypeDef *htim,
+				uint32_t PWM_Channel, GPIO_TypeDef* Port,uint16_t IN0, uint16_t IN1){
+	motor->max_speed = max_speed;
+	motor->htim = htim;
+	motor->PWM_Channel = PWM_Channel;
+	motor->Port = Port;
+	motor->IN0 = IN0;
+	motor->IN1 = IN1;
+	motor->pulse = 0;
+	motor->state = 0;
+
+
+}
+
+void DCMotor_Set_Speed(DCMotor *motor,uint16_t DutyCycle){
+	 DutyCycle = DutyCycle * motor->htim->Instance->ARR/100;
+	__HAL_TIM_SET_COMPARE(motor->htim,motor->PWM_Channel, DutyCycle);
+}
+
+void DCMotor_Run(DCMotor *motor, uint8_t direction){
+	if(direction == 1){
+		HAL_GPIO_WritePin(motor->Port, motor->IN0, SET);
+		HAL_GPIO_WritePin(motor->Port, motor->IN1, RESET);
+	}
+	else if(direction == -1){
+		HAL_GPIO_WritePin(motor->Port, motor->IN0, RESET);
+		HAL_GPIO_WritePin(motor->Port, motor->IN1, SET);
+	}
+	else{
+		HAL_GPIO_WritePin(motor->Port, motor->IN0, RESET);
+		HAL_GPIO_WritePin(motor->Port, motor->IN1, RESET);
+	}
+}
