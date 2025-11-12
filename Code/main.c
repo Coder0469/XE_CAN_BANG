084422b44b19966203bdf80923e3e352ef376e47
Đình Hiếu <dinhhieu8061@gmail.com>
2025-11-12 10:21:42 +0700

    Update main.c

--- Code/main.c
+++ Code/main.c
@@ -1,510 +1,188 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "usb_device.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-#include <string.h>
-#include <math.h>
 #include "usbd_cdc_if.h"
-/* USER CODE END Includes */
+#include <string.h>
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-typedef struct{
-	int max_speed;
-	TIM_HandleTypeDef *htim;
-	uint32_t PWM_Channel;
-	uint16_t IN0;
-	uint16_t IN1;
-	uint16_t input;
-	uint16_t exti;
-	int pulse;
-	uint8_t state;
-	GPIO_TypeDef* Port;
 
-} DCMotor;
-/* USER CODE END PTD */
+#define FLASH_USER_START_ADDR   0x080E0000  // Sector 11 start
+#define FLASH_USER_END_ADDR     0x080FFFFF  // Sector 11 end
+#define FLASH_PAGE_SIZE         0x20000     // 128 KB per sector (sector 11)
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
 
-/* USER CODE END PD */
+uint32_t Flash_Write_Address = FLASH_USER_START_ADDR;
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
 
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-TIM_HandleTypeDef htim1;
-TIM_HandleTypeDef htim3;
-TIM_HandleTypeDef htim4;
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_TIM3_Init(void);
-static void MX_TIM4_Init(void);
-static void MX_TIM1_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-DCMotor MotorA;
-char buffer[100];
-int t = 0;
-int speed = 0;
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-
-}
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-//	if(GPIO_Pin == MotorA.exti){
-//		if(HAL_GPIO_ReadPin(MotorA.Port, MotorA.input) == 0){
-//			MotorA.pulse--;
-//		}
-//		else{
-//			MotorA.pulse++;
-//		}
-//	}
-	MotorA.pulse++;
-}
-
-void DCMotor_Init(DCMotor *motor,int max_speed, TIM_HandleTypeDef *htim,
-				uint32_t PWM_Channel, GPIO_TypeDef* Port,uint16_t IN0, uint16_t IN1){
-	motor->max_speed = max_speed;
-	motor->htim = htim;
-	motor->PWM_Channel = PWM_Channel;
-	motor->Port = Port;
-	motor->IN0 = IN0;
-	motor->IN1 = IN1;
-	motor->pulse = 0;
-	motor->state = 0;
+static void MX_SPI1_Init(void);
+void Process_USB_Command(uint8_t* buf, uint32_t len);
+uint32_t Flash_Write_Data(uint8_t* data, uint32_t len);
+void Flash_Read_Data(void);
+void Flash_Erase(void);
 
 
-}
-
-void DCMotor_Set_Speed(DCMotor *motor,int DutyCycle){
-	 DutyCycle = DutyCycle * motor->htim->Instance->ARR/100;
-	__HAL_TIM_SET_COMPARE(motor->htim,motor->PWM_Channel,(uint16_t) DutyCycle);
-}
-
-void DCMotor_Run(DCMotor *motor, uint8_t direction){
-	if(direction == 1){
-		HAL_GPIO_WritePin(motor->Port, motor->IN0, SET);
-		HAL_GPIO_WritePin(motor->Port, motor->IN1, RESET);
-	}
-	else if(direction == -1){
-		HAL_GPIO_WritePin(motor->Port, motor->IN0, RESET);
-		HAL_GPIO_WritePin(motor->Port, motor->IN1, SET);
-	}
-	else{
-		HAL_GPIO_WritePin(motor->Port, motor->IN0, RESET);
-		HAL_GPIO_WritePin(motor->Port, motor->IN1, RESET);
-	}
-}
-
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-
-  /* USER CODE BEGIN 1 */
-
-
-
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_TIM3_Init();
-  MX_TIM4_Init();
-  MX_TIM1_Init();
+  MX_SPI1_Init();
   MX_USB_DEVICE_Init();
-  /* USER CODE BEGIN 2 */
-//  HAL_TIM_Base_Start_IT(&htim3);
-//  HAL_TIM_Base_Start_IT(&htim4);
-  /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  DCMotor_Init(&MotorA, 60, &htim3, TIM_CHANNEL_1, GPIOA, GPIO_PIN_3, GPIO_PIN_7);
-  DCMotor_Set_Speed(&MotorA, 100);
-  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
-  int pulse_now = 0;
-  int pulse_before = 0;
-  int delta = 0;
-  float tmp = 0.12;
   while (1)
   {
-	pulse_now = MotorA.pulse;
-	delta = pulse_now - pulse_before;
-	pulse_before = pulse_now;
-	snprintf(buffer, sizeof(buffer), "Pulse count: %d speed: %d\r\n", MotorA.pulse, delta*12/100);
-	CDC_Transmit_FS((uint8_t*)buffer, strlen(buffer));
-
-	HAL_Delay(1000);
-	DCMotor_Run(&MotorA, 1);
-
-//	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, SET);
-
-
-
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+      HAL_Delay(10);
   }
-  /* USER CODE END 3 */
 }
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
+uint32_t Flash_Write_Data(uint8_t* data, uint32_t len)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    HAL_FLASH_Unlock();
+    uint32_t bytes_to_write = len;
+    uint32_t bytes_written = 0; // Biến đếm số byte đã ghi thành công
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    if (Flash_Write_Address + bytes_to_write > FLASH_USER_END_ADDR) {
+        bytes_to_write = FLASH_USER_END_ADDR - Flash_Write_Address + 1;
+    }
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    for(uint32_t i = 0; i < bytes_to_write; i++)
+    {
+        if(HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE, Flash_Write_Address, data[i]) == HAL_OK)
+        {
+            Flash_Write_Address++;
+            bytes_written++; // Ghi thành công
+        }
+        else
+        {
+            // Lỗi ghi xảy ra, dừng và trả về số byte đã ghi được
+            break;
+        }
+    }
+    HAL_FLASH_Lock();
+    return bytes_written; // Trả về số byte đã ghi thành công
 }
-
-/**
-  * @brief TIM1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM1_Init(void)
+void Flash_Read_Data(void)
 {
+    uint32_t addr = FLASH_USER_START_ADDR;
+    uint8_t buf[64];
+    uint32_t cnt = 0;
 
-  /* USER CODE BEGIN TIM1_Init 0 */
+    while ((addr < FLASH_USER_END_ADDR))
+    {
+        uint8_t byte = *(__IO uint8_t *)addr++;
+        if (byte == '\0' || byte == 0xFF) break; // Dừng khi gặp ký tự kết thúc hoặc vùng trống
+        buf[cnt++] = byte;
 
-  /* USER CODE END TIM1_Init 0 */
+        if (cnt == 64)
+        {
+            CDC_Transmit_FS(buf, 64);
+            cnt = 0;
+        }
+    }
 
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TIM1_Init 1 */
-
-  /* USER CODE END TIM1_Init 1 */
-  htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 16799;
-  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 9999;
-  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim1.Init.RepetitionCounter = 0;
-  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM1_Init 2 */
-
-  /* USER CODE END TIM1_Init 2 */
-
+    if (cnt > 0)
+        CDC_Transmit_FS(buf, cnt);
+    else
+    	CDC_Transmit_FS("FLASH EMPTY\n",strlen("FLASH EMPTY\n"));
 }
-
-/**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM3_Init(void)
+void Flash_Erase(void)
 {
+    HAL_FLASH_Unlock();
 
-  /* USER CODE BEGIN TIM3_Init 0 */
+    FLASH_EraseInitTypeDef EraseInitStruct;
+    uint32_t SectorError;
 
-  /* USER CODE END TIM3_Init 0 */
+    EraseInitStruct.TypeErase = FLASH_TYPEERASE_SECTORS;
+    EraseInitStruct.Sector = FLASH_SECTOR_11;
+    EraseInitStruct.NbSectors = 1;
+    EraseInitStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3; // 2.7 ~ 3.6V
 
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
+    if(HAL_FLASHEx_Erase(&EraseInitStruct, &SectorError) != HAL_OK)
+    {
+     CDC_Transmit_FS((uint8_t*)"ERASE FAILED! SECTOR NOT CLEAR!\r\n", 33);
+    }
 
-  /* USER CODE BEGIN TIM3_Init 1 */
+    HAL_FLASH_Lock();
 
-  /* USER CODE END TIM3_Init 1 */
-  htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 0;
-  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 65535;
-  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM3_Init 2 */
-
-  /* USER CODE END TIM3_Init 2 */
-  HAL_TIM_MspPostInit(&htim3);
-
+    Flash_Write_Address = FLASH_USER_START_ADDR;
 }
+void Process_USB_Command(uint8_t* buf, uint32_t len){
+	if(len>=6 && strncmp((char*)buf, "WRITE ", 6)==0)
+     {
+         uint32_t data_len = len - 6;
+         uint32_t written_len = Flash_Write_Data(buf + 6, data_len);
 
-/**
-  * @brief TIM4 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM4_Init(void)
-{
+         // Ghi ký tự kết thúc '\0'
+         uint8_t null_byte = '\0';
+         Flash_Write_Data(&null_byte, 1);
+// Bắt đầu logic báo cáo lỗi ghi
+            	if (written_len < data_len)
+                  {
+                      // Chuyển đổi số thành chuỗi để báo cáo lỗi
+                      char msg_buffer[64];
+                      int required_len = snprintf(msg_buffer, 64, "WRITE FAILED! Chỉ ghi được %lu/%lu byte.\r\n",
+                                                  written_len, data_len);
+                      CDC_Transmit_FS((uint8_t*)msg_buffer, (uint32_t)required_len);
+                  }
+                  else
+                  {
+                      CDC_Transmit_FS((uint8_t*)"Write complete\r\n", strlen("Write complete\r\n"));
+                  }
+              }
+        else if(len>=4 && strncmp((char*)buf, "READ ", 4)==0)
+             {
+                 Flash_Read_Data();
+                 char msg[] = "\r\n";
+                 CDC_Transmit_FS((uint8_t*)msg, strlen(msg));
+             }
+        else if(len>=5 && strncmp((char*)buf, "ERASE", 5)==0)
+             {
+                 Flash_Erase();
+                 char msg[] = "\r\n";
+                 CDC_Transmit_FS((uint8_t*)"Erase complete\r\n", strlen("Erase complete\r\n"));
+                 CDC_Transmit_FS((uint8_t*)msg, strlen(msg));
+             }
+        else
+             {
+              CDC_Transmit_FS((uint8_t*)"MISTAKE\r\n", strlen("MISTAKE\r\n"));
+             }
+         }
 
-  /* USER CODE BEGIN TIM4_Init 0 */
+void SystemClock_Config(void){
+           RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+           RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /* USER CODE END TIM4_Init 0 */
+           __HAL_RCC_PWR_CLK_ENABLE();
+           __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
+           RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+           RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+           RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+           RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+           RCC_OscInitStruct.PLL.PLLM = 8;
+           RCC_OscInitStruct.PLL.PLLN = 336;
+           RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+           RCC_OscInitStruct.PLL.PLLQ = 7;
+           if(HAL_RCC_OscConfig(&RCC_OscInitStruct)!=HAL_OK) Error_Handler();
+           RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK|
+                                         RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+           RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+           RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+           RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+           RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+           if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5)!=HAL_OK) Error_Handler();
+         }
+         static void MX_GPIO_Init(void)
+         {
+             __HAL_RCC_GPIOH_CLK_ENABLE();
+             __HAL_RCC_GPIOA_CLK_ENABLE();
+             __HAL_RCC_GPIOB_CLK_ENABLE();
+         }
 
-  /* USER CODE BEGIN TIM4_Init 1 */
-
-  /* USER CODE END TIM4_Init 1 */
-  htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 0;
-  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 65535;
-  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM4_Init 2 */
-
-  /* USER CODE END TIM4_Init 2 */
-  HAL_TIM_MspPostInit(&htim4);
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_1, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3|GPIO_PIN_7, GPIO_PIN_RESET);
-
-  /*Configure GPIO pins : PC0 PC1 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PA1 PA5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_5;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PA2 PA4 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_4;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PA3 PA7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_7;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+         static void MX_SPI1_Init(void)
+         {
+         }
 void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-#ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
+         {
+           __disable_irq();
+           while(1);
+         }
